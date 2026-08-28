53e87ec01ec573b80fde18e3ba5bdd971011bfe9
Aman Jha <75173703+inofficialamanjha@users.noreply.github.com>
2026-08-29 01:59:05 +0530

    Add: sqrtx - submission-9

---
+++ Data Structures & Algorithms/sqrtx/submission-9.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long low = 0, high = x;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+
+            if (mid * mid <= x)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return high;
+    }
+};
