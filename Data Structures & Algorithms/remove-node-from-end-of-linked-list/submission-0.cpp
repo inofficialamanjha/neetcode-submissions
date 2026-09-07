168e852dd9beacc3d400c97fbc5b38091a6d4489
Aman Jha <75173703+inofficialamanjha@users.noreply.github.com>
2026-09-07 23:47:39 +0530

    Add: remove-node-from-end-of-linked-list - submission-0

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    ListNode* reverseLinkedList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            ListNode* next = curr->next;
+
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        // 1. Reverse the list
+        head = reverseLinkedList(head);
+
+        // 2. Remove nth node from the beginning
+        //    (which is nth from the end in original list)
+
+        if (n == 1) {
+            head = head->next;
+        } 
+        else {
+            ListNode* curr = head;
+
+            // Move to node just before nth node
+            for (int i = 1; i < n - 1; i++) {
+                curr = curr->next;
+            }
+
+            curr->next = curr->next->next;
+        }
+
+        // 3. Reverse again
+        head = reverseLinkedList(head);
+
+        return head;
+    }
+};
