fc207740c9cc7e5913003f059a632800018a910e
Aman Jha <75173703+inofficialamanjha@users.noreply.github.com>
2026-09-13 02:15:47 +0530

    Add: find-median-in-a-data-stream - submission-3

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-3.cpp
@@ -0,0 +1,33 @@
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    MedianFinder() {
+    }
+    
+    void addNum(int num) {
+
+        // Add to left half
+        maxHeap.push(num);
+
+        // Move largest from left to right
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        // Keep maxHeap equal or one larger
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.top();
+        }
+
+        return (maxHeap.top() + minHeap.top()) / 2.0;
+    }
+};
