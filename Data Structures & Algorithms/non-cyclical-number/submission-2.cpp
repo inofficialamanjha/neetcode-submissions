51b8d07f5a5a31fd462a84fb8ba7924955510009
Aman Jha <75173703+inofficialamanjha@users.noreply.github.com>
2026-09-13 02:36:41 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = sumDigitSquare(slow);
+            fast = sumDigitSquare(sumDigitSquare(fast));
+        } while(slow!=fast);
+
+        return slow==1;
+    }
+
+    int sumDigitSquare(int n) {
+        int sum = 0;
+        while(n!=0) {
+            sum += (n%10)*(n%10);
+            n = n/10;
+        }
+        return sum; 
+    }
+};
